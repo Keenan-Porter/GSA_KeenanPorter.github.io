ee6665c6c909cef0b55dc6044e846d0b4bbf94fe
Keenan-Porter <116773589+Keenan-Porter@users.noreply.github.com>
2026-04-02 19:15:23 -0400

    Init

---
+++ Assignment_2.js
@@ -0,0 +1,25 @@
+import { default as gulls } from 'https://charlieroberts.codeberg.page/gulls/gulls.js'
+
+// start seagulls, by default it will use the first <canvas> element it
+// finds in your HTML page
+const sg   = await gulls.init()
+
+// a simple vertex shader to make a quad
+const quadVertexShader = gulls.constants.vertex
+
+// our fragment shader, just returns blue
+const fragmentShader = `
+@fragment
+fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
+  return vec4(0., 0., 1., 1. );
+}
+`
+
+// our vertex + fragment shader together
+const shader = quadVertexShader + fragmentShader
+
+// create a render pass
+const renderPass = await sg.render({ shader })
+
+// run our render pass
+sg.run( renderPass )

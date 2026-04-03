05723b512e85c43d10db31f8fafd0af78e08ce8b
Keenan-Porter <116773589+Keenan-Porter@users.noreply.github.com>
2026-04-02 23:44:19 -0400

    Prior to slider in tutorial

--- main.js
+++ main.js
@@ -9,17 +9,31 @@ const quadVertexShader = gulls.constants.vertex
 
 // our fragment shader, just returns blue
 const fragmentShader = `
-@fragment
-fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
-  return vec4(0., 0., 1., 1. );
-}
+  @group(0) @binding(0) var<uniform> res   : vec2f;
+  @group(0) @binding(1) var<uniform> frame : f32;
+  
+  @fragment
+  fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
+    let uv = pos.xy / res;
+    return vec4f( uv.x, .5+sin(frame/60.)*.5, uv.y, 1. );
+  }
 `
 
 // our vertex + fragment shader together
 const shader = quadVertexShader + fragmentShader
 
 // create a render pass
-const renderPass = await sg.render({ shader })
+let frame = sg.uniform( 0 )
+const renderPass = await sg.render({ 
+  shader,
+  // add a data array to specify uniforms / buffers / textures etc.
+  data:[
+    sg.uniform([ window.innerWidth, window.innerHeight ]),
+    // make sure this is second so it gets bound to index 1
+    frame
+  ],
+  onframe() { frame.value++ }
+})
 
 // run our render pass
 sg.run( renderPass )
